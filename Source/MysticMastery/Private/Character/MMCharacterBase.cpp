6146ab644df0115e005a18f680aa7d6fdc3f0220
fonyc <fonycas@hotmail.com>
2023-09-22 14:52:29 +0200

    folder structure

---
+++ Source/MysticMastery/Private/Character/MMCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MysticMastery/Public/MMCharacterBase.h"
+
+
+// Sets default values
+AMMCharacterBase::AMMCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMMCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}

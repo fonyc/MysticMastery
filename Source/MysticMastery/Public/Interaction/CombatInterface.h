8d8e48e558a6f2171b2f42a31e0b9343da003559
fonyc <fonycas@hotmail.com>
2023-12-06 01:19:00 +0100

    motion warp removed dependency with character and added it with combat interface

--- Source/MysticMastery/Public/Interaction/CombatInterface.h
+++ Source/MysticMastery/Public/Interaction/CombatInterface.h
@@ -1,13 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
-// This class does not need to be modified.
-UINTERFACE()
+UINTERFACE(BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -20,8 +17,10 @@ class MYSTICMASTERY_API ICombatInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	virtual int32 GetPlayerLevel() const;
 	virtual FVector GetCombatSocketLocation();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void UpdateFacingWarpTarget(const FVector& Target);
 };

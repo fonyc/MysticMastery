8c1daf6beeba0441b4e7e715942d3a9fecf47fa2
fonyc <fonycas@hotmail.com>
2024-01-04 21:26:54 +0100

    added basic movement AI with BTS no BP based

---
+++ Source/MysticMastery/Private/AI/MMBTService_FindNearestPlayer.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/MMBTService_FindNearestPlayer.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMMBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	AAIController* Controller = OwnerComp.GetAIOwner();
+	const APawn* OwningPawn = Controller->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTargetTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTargetTag);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	
+	for (AActor* NewActor : ActorsWithTargetTag)
+	{
+		const float NewDistance = OwningPawn->GetDistanceTo(NewActor);
+		if (IsValid(NewActor) && IsValid(OwningPawn))
+		{
+			if (NewDistance < ClosestDistance)
+			{
+				ClosestDistance = NewDistance;
+				ClosestActor = NewActor;
+			}
+		}
+	}
+	
+	Controller->GetBlackboardComponent()->SetValueAsObject(TargetToFollowSelector.SelectedKeyName, ClosestActor);
+	Controller->GetBlackboardComponent()->SetValueAsFloat(DistanceToTargetSelector.SelectedKeyName, ClosestDistance);
+}
---
+++ Source/MysticMastery/Public/AI/MMBTService_FindNearestPlayer.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "MMBTService_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSTICMASTERY_API UMMBTService_FindNearestPlayer : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector DistanceToTargetSelector;
+};
